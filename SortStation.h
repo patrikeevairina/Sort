0b06f73c0612cd20e1c08d2f327c45efec95de84
patrikeevairina <59138641+patrikeevairina@users.noreply.github.com>
2020-05-25 03:27:48 +0300

    Update SortStation.h

--- SortStation.h
+++ SortStation.h
@@ -1,4 +1,21 @@
 #ifndef SORTSTATION_H
 #define SORTSTATION_H
 
+#include "BracketToken.h"
+#include "FunctionToken.h"
+#include "NumberToken.h"
+#include "OperatorToken.h"
+
+#include "Token.h"
+
+class SortStation
+{
+public:
+    explicit SortStation(std::string& str);
+    double getResult();
+    
+private:
+    Token *_token;
+};
+
 #endif // SORTSTATION_H
