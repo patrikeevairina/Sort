2c3cab530a04e734a59b8b2f2cdc1f167e0ec865
patrikeevairina <59138641+patrikeevairina@users.noreply.github.com>
2020-05-25 03:28:52 +0300

    Update NumberToken.h

--- NumberToken.h
+++ NumberToken.h
@@ -1,4 +1,26 @@
 #ifndef NUMBERTOKEN_H
 #define NUMBERTOKEN_H
 
+#include "Token.h"
+
+class NumberToken : public Token
+{
+public:
+    TokenType type() override;
+    double value();
+private:
+    TokenType _type;
+    double _value;
+};
+
+TokenType NumberToken::type()
+{
+    return _type;
+}
+
+double NumberToken::value()
+{
+    return _value;
+}
+
 #endif // NUMBERTOKEN_H
