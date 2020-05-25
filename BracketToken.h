ed988c9ad7e01ff6e6c2c64528ff9d6a2509f064
patrikeevairina <59138641+patrikeevairina@users.noreply.github.com>
2020-05-25 03:29:30 +0300

    Update BracketToken.h

--- BracketToken.h
+++ BracketToken.h
@@ -1,4 +1,33 @@
 #ifndef BRACKETTOKEN_H
 #define BRACKETTOKEN_H
 
+#include "Token.h"
+
+enum class Orientation
+{
+    Left,
+    Right
+};
+
+class BracketToken : public Token
+{
+public:
+    TokenType type() override;
+    Orientation orientation();
+    
+private:
+    TokenType _type;
+    Orientation _orientation;
+};
+
+TokenType BracketToken::type()
+{
+    return _type;
+}
+
+Orientation BracketToken::orientation()
+{
+    return _orientation;
+}
+
 #endif // BRACKETTOKEN_H
