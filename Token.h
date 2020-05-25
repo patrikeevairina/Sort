5c1378e7d2a5e147c59eb5a442e32243d3a436d2
patrikeevairina <59138641+patrikeevairina@users.noreply.github.com>
2020-05-25 03:27:17 +0300

    Update Token.h

--- Token.h
+++ Token.h
@@ -1,4 +1,25 @@
 #ifndef TOKEN_H
 #define TOKEN_H
 
+#include <iostream>
+#include <cstring> 
+
+enum class TokenType
+{
+    Number,
+    Operator,
+    Function, 
+    Bracket
+};
+
+
+class Token
+{
+public:
+    virtual TokenType type() = 0;
+    ~Token();
+protected:
+    TokenType _type;
+};
+
 #endif // TOKEN_H
